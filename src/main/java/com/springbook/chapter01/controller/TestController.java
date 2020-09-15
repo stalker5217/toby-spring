4a5ec6d5e901f40c64c9f79513e4029342e33de3
Song <stalker5217@gmail.com>
2020-09-15 12:52:33 +0900

    스프링의 IoC

--- src/main/java/com/springbook/chapter01/controller/TestController.java
+++ src/main/java/com/springbook/chapter01/controller/TestController.java
@@ -8,7 +8,10 @@ import com.springbook.chapter01.dao.s3.NConnectionMaker3;
 import com.springbook.chapter01.dao.s3.UserDao3;
 import com.springbook.chapter01.dao.s4.DaoFactory4;
 import com.springbook.chapter01.dao.s4.UserDao4;
+import com.springbook.chapter01.dao.s5.DaoFactory5;
 import com.springbook.chapter01.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -83,6 +86,28 @@ public class TestController {
     @GetMapping("/chapter01/s4/userdao")
     public String userdao4() throws SQLException, ClassNotFoundException {
         UserDao4 dao = new DaoFactory4().userDao();
+
+        User user = new User();
+        user.setId("stalker");
+        user.setName("스토커");
+        user.setPassword("1234");
+
+        dao.add(user);
+        System.out.println(user.getId() + " 등록 성공!");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공!");
+
+        return user2.getId();
+    }
+
+    @GetMapping("/chapter01/s5/userdao")
+    public String userdao5() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory5.class);
+        UserDao4 dao = context.getBean("userDao", UserDao4.class);
 
         User user = new User();
         user.setId("stalker");
